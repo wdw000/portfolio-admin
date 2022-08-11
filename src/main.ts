ca9dcc599ec07488b68ada4a1cfb3b69bf856985
wdw000 <dowonwang01@gmail.com>
2022-08-11 22:05:46 +0900

    init

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import axios from "axios";
 const pinia = createPinia();
 
 export const instance = axios.create({
-  baseURL: process.env.VUE_APP_API_URL,
+  baseURL: "https://wangdo.site/api/portfolio",
 });
 
 createApp(App).use(pinia).mount("#app");
