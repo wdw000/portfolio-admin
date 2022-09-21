397cbd18b774d20a5cb31ed7ea3bb042d972f348
wdw000 <dowonwang01@gmail.com>
2022-09-21 22:02:38 +0900

    modified title

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,10 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: "/admin",
+  pages: {
+    index: {
+      title: "Portfolio Admin",
+      entry: "src/main.ts",
+    },
+  },
 });
